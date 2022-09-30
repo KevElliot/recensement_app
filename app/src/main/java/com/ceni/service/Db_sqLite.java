afaaa104ef803cee9d0f5f9b0cd90b0c3ab98d03
FreakAfo <46443733+mahefaRav@users.noreply.github.com>
2022-09-30 22:19:40 +0300

    commit

--- app/src/main/java/com/ceni/service/Db_sqLite.java
+++ app/src/main/java/com/ceni/service/Db_sqLite.java
@@ -30,7 +30,7 @@ import java.util.List;
 public class Db_sqLite extends SQLiteOpenHelper {
     private Context context;
     private static final String DB_NAME = "Recensement.db";
-    private static final int DB_VERSION = 39;
+    private static final int DB_VERSION = 44;
     /*---------------------------------------------------------------------------------------
                                            TABLE ELECTEUR
     ----------------------------------------------------------------------------------------*/
