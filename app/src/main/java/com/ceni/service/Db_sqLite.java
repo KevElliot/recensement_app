27dbc7dcadd4bd7f3b02742935248e4479ad4eb1
unknown <r.kevinelliot@gmail.com>
2022-10-03 12:16:13 +0300

    commit

--- app/src/main/java/com/ceni/service/Db_sqLite.java
+++ app/src/main/java/com/ceni/service/Db_sqLite.java
@@ -196,7 +196,7 @@ public class Db_sqLite extends SQLiteOpenHelper {
     public Boolean isSamePerson(String nom, String prenom, String dateNaiss, String codebv) {
         boolean result = false;
         SQLiteDatabase MyDB = this.getWritableDatabase();
-        String sql = "Select upper(nom),upper(prenom),dateNaiss,code_bv from electeur where (upper(nom) =upper('" + nom + "') and upper(prenom) = upper('" + prenom + "') and dateNaiss = '" + dateNaiss + "' and code_bv ='"+ codebv+"') or (upper(nom)= upper('" + prenom + "') and upper(prenom) = upper('" + nom + "') and dateNaiss = '" + dateNaiss + "'and code_bv ='"+ codebv+"')";
+        String sql = "Select upper(nom),upper(prenom),dateNaiss,code_bv from electeur where (upper(nom) =upper('" + nom + "') and upper(prenom) = upper('" + prenom + "') and dateNaiss = '" + dateNaiss + "') or (upper(nom)= upper('" + prenom + "') and upper(prenom) = upper('" + nom + "') and dateNaiss = '" + dateNaiss + "')";
         Log.d("isSame", sql);
         Cursor cursor = MyDB.rawQuery(sql, new String[]{});
         try {
